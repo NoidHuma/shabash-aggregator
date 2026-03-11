9b4ffea836304e93bddffffabb72a5967414eaaf
NoidHuma <noidhuma0@gmail.com>
2026-03-11 14:38:24 +0300

    connection (database.py)

---
+++ app/db/database.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+from app.core.config import settings
+
+
+DATABASE_URL = (
+    f"postgresql+asyncpg://"
+    f"{settings.postgres_user}:"
+    f"{settings.postgres_password}@"
+    f"{settings.postgres_host}:"
+    f"{settings.postgres_port}/"
+    f"{settings.postgres_db}"
+)
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True
+)
+
+SessionLocal = async_sessionmaker(
+    engine,
+    expire_on_commit=False
+)
+
+
+class Base(DeclarativeBase):
+    pass
