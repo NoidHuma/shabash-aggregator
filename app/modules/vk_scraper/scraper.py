4e32383d172f36633a5628af231f57f02b81be05
NoidHuma <noidhuma0@gmail.com>
2026-03-15 20:19:33 +0300

    add vk text existence checking

--- app/modules/vk_scraper/scraper.py
+++ app/modules/vk_scraper/scraper.py
@@ -1,11 +1,26 @@
-# app/modules/vk_scraper/scraper.py
+from app.modules.vk_scraper.vk_client import VKClient
 
 
 class VKScraper:
-    """
-    Основной класс VK-скрапера.
 
-    Пока содержит только каркас.
-    """
+    def __init__(
+        self,
+        vk_client: VKClient,
+    ) -> None:
+        self._vk_client = vk_client
 
-    pass
+    async def process_group(
+        self,
+        owner_id: int,
+    ) -> None:
+
+        posts = await self._vk_client.get_latest_posts(
+            owner_id=owner_id,
+        )
+
+        for post in posts:
+
+            text = post.get("text")
+
+            if not text:
+                continue
