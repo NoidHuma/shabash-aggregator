c6d7ef01bddbc33b43bae7b97d84d7b2cda9ed95
NoidHuma <noidhuma0@gmail.com>
2026-03-17 21:15:33 +0300

    add deduplication

--- app/modules/vk_scraper/scraper.py
+++ app/modules/vk_scraper/scraper.py
@@ -1,17 +1,22 @@
 from app.modules.vk_scraper.vk_client import VKClient
+from app.repositories.posts_repository import PostsRepository
+from app.services.hash_service import calculate_text_hash
 
 
 class VKScraper:
 
     def __init__(
-        self,
-        vk_client: VKClient,
+            self,
+            vk_client: VKClient,
+            posts_repository: PostsRepository,
     ) -> None:
         self._vk_client = vk_client
+        self._posts_repository = posts_repository
 
     async def process_group(
-        self,
-        owner_id: int,
+            self,
+            session,
+            owner_id: int,
     ) -> None:
 
         posts = await self._vk_client.get_latest_posts(
@@ -26,4 +31,16 @@ class VKScraper:
                 continue
 
             if post.get("marked_as_ads") == 1:
+                continue
+
+            text_hash = calculate_text_hash(text)
+
+            is_duplicate = (
+                await self._posts_repository.exists_hash_last_hour(
+                    session=session,
+                    text_hash=text_hash,
+                )
+            )
+
+            if is_duplicate:
                 continue
