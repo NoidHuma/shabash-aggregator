d5887acb3c4ced4ecb5bdf021247d2cb9ba5b511
NoidHuma <noidhuma0@gmail.com>
2026-03-17 22:18:52 +0300

    refactor redis settings

--- app/core/redis.py
+++ app/core/redis.py
@@ -1,8 +1,10 @@
 import redis.asyncio as redis
 
+from app.core.config import settings
+
 
 redis_client = redis.Redis(
-    host="localhost",
-    port=6379,
+    host=settings.redis_host,
+    port=settings.redis_port,
     decode_responses=True
 )
